caa9437bd029afcfe5bb52c0720ddfb593b19d90
maros.klimovsky <395858@mail.muni.cz>
2013-10-07 23:35:11 +0200

    Project maven skeleton

---
+++ SoccerRecords/src/main/java/cz/muni/pa165/magic4/soccerrecords/App.java
@@ -0,0 +1,13 @@
+package cz.muni.pa165.magic4.soccerrecords;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
