efab2e8dcefb181608b8f3a70afd94c373aa02b4
maros-klimovsky <395858@mail.muni.cz>
2013-10-09 16:20:12 +0200

    PlayerDao correction
    - removed from obsolete package dao
    + added javadoc

--- src/main/java/cz/muni/fi/pa165/mamatoad/soccerrecords/player/PlayerDao.java
+++ src/main/java/cz/muni/fi/pa165/mamatoad/soccerrecords/player/PlayerDao.java
@@ -4,23 +4,58 @@ import cz.muni.fi.pa165.mamatoad.soccerrecords.team.Team;
 import java.util.List;
 
 /**
- *
- * @author maros-klimovsky
+ * This interface provides create, retrieve, update and delete methods for work with Player entity.
+ * @author Maros Klimovsky
  */
 public interface PlayerDao {
     
-    Player createPlayer(Player player);
+    /**
+     * This method creates new Player
+     * @param player to create
+     * @return created player
+     */
+    Player create(Player player);
     
-    Player updatePlayer(Player player);
+    /**
+     * This method updates player
+     * @param player to update
+     * @return updated player
+     */
+    Player update(Player player);
     
-    void deletePlayer(Player player);
+    /**
+     * This method deletes player
+     * @param player to delete
+     */
+    void delete(Player player);
     
+    /**
+     * This method retrieves player by his id
+     * @param id of player
+     * @return player with given id
+     */
     Player retrievePlayerById(Long id);
     
+    /**
+     * This method retrieves player by name
+     * @param name of player
+     * @return list of players with given name
+     */
     List<Player> retrievePlayersByName(String name);
     
+    /**
+     * This method retrieves player by team
+     * @param team of player
+     * @return list of players with given team
+     */
     List<Player> retrievePlayersByTeam(Team team);
     
+    /**
+     * This method retrieves player by activity
+     * @param active - is player active?
+     * @return list of players with given activity
+     */
     List<Player> retrievePlayersByActivity(Boolean active);
+    
     
 }
