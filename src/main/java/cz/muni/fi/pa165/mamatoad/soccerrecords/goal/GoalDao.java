46a6b606944e5a6b59f54957dde52c1088d7415b
Adriana <adriana.smijakova@gmail.com>
2013-10-09 18:10:00 +0200

    Added javadoc in GoalDao.java

--- src/main/java/cz/muni/fi/pa165/mamatoad/soccerrecords/goal/GoalDao.java
+++ src/main/java/cz/muni/fi/pa165/mamatoad/soccerrecords/goal/GoalDao.java
@@ -6,25 +6,84 @@ import cz.muni.fi.pa165.mamatoad.soccerrecords.team.Team;
 import java.util.List;
 
 /**
+ * This interface allows to manipulate with goals.
  *
- * @author adriana-smijakova
+ * @author Adriana Smijakova
  */
 public interface GoalDao {
     
-    Goal createGoal(Goal goal);
     
-    Goal updateGoal(Goal goal);
+    /**
+     * Stores new goal into database.
+     * 
+     * @param goal to be created
+     * @return created goal
+     * @exception IllegalArgumentException when goal is null
+     */
+    Goal createGoal(Goal goal)throws IllegalArgumentException;
     
-    void deleteGoal(Goal goal);
+    /**
+     * Updates gived goal in database.
+     * 
+     * @param goal to be updated
+     * @return updated goal
+     * @exception IllegalArgumentException when goal is null
+     */
+    Goal updateGoal(Goal goal) throws IllegalArgumentException;
     
-    Goal retrieveGoalById(Long id);
+    /**
+     * Deletes given goal in database.
+     * 
+     * @param goal to be deleted 
+     * @exception  IllegalArgumentException when goal is null
+     */
+    void deleteGoal(Goal goal) throws IllegalArgumentException;
     
-    List<Goal> retrieveGoalsByMatch(Match match);
+    /**
+     * Retrieves goal with given id.
+     * 
+     * @param id primary key of requested goal
+     * @return goal with given id or null if such goal does not exist
+     * @exception IllegalArgumentException when id is null
+     */
+    Goal retrieveGoalById(Long id) throws IllegalArgumentException;
     
-    List<Goal> retrieveGoalsByMatchAndTeam(Match match, Team team);
+    /**
+     * Retrieves goals in given match.
+     * 
+     * @param match where were goals shot
+     * @return list of goals with given match
+     * @exception IllegalArgumentException when match is null
+     */
+    List<Goal> retrieveGoalsByMatch(Match match) throws IllegalArgumentException;
     
-    List<Goal> retrieveGoalsByPlayer(Player player);
+    /**
+     * Retrieves goals in given match shot by given team.
+     * 
+     * @param match where were goals shot
+     * @param team which shot the goals 
+     * @return list of goals with given match and team
+     * @exception  IllegalArgumentException when match or team is null
+     */
+    List<Goal> retrieveGoalsByMatchAndTeam(Match match, Team team) throws IllegalArgumentException;
     
-    List<Goal> retrieveGoalsByMatchAndPlayer(Match match, Player player);
+    /**
+     * Retrieves goals shot by given player
+     * 
+     * @param player who shot the goals
+     * @return list of goals witch given player
+     * @exception IllegalArgumentException when player is null
+     */
+    List<Goal> retrieveGoalsByPlayer(Player player) throws IllegalArgumentException;
+    
+    /**
+     * Retrieves goals shot in given match by given player
+     * 
+     * @param match where were goals shot
+     * @param player who shot the goals
+     * @return list of goals witch goven match and player
+     * @exception IllegalArgumentException when match or player is null
+     */
+    List<Goal> retrieveGoalsByMatchAndPlayer(Match match, Player player) throws IllegalArgumentException;
     
 }
