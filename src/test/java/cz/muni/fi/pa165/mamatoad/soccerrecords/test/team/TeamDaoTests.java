e077d3b0830ad2d16c524b5969fe13e8029eb394
Adriana <adriana.smijakova@gmail.com>
2013-10-10 19:38:14 +0200

    Added new Test structure and TeamDaoTest test

---
+++ src/test/java/cz/muni/fi/pa165/mamatoad/soccerrecords/test/team/TeamDaoTests.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.mamatoad.soccerrecords.test.team;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Adriana
+ */
+public class TeamDaoTests {
+    
+    public TeamDaoTests() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
