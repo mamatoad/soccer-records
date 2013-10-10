de89915da05558e494836a9cb7eedb12a7f12045
Adriana <adriana.smijakova@gmail.com>
2013-10-10 18:06:32 +0200

    Added IllegalEntityException to GoalDao methods

--- src/main/java/cz/muni/fi/pa165/mamatoad/soccerrecords/goal/GoalDao.java
+++ src/main/java/cz/muni/fi/pa165/mamatoad/soccerrecords/goal/GoalDao.java
@@ -3,6 +3,7 @@ package cz.muni.fi.pa165.mamatoad.soccerrecords.goal;
 import cz.muni.fi.pa165.mamatoad.soccerrecords.match.Match;
 import cz.muni.fi.pa165.mamatoad.soccerrecords.player.Player;
 import cz.muni.fi.pa165.mamatoad.soccerrecords.team.Team;
+import cz.muni.fi.pa165.mamatoad.soccerrecords.util.exception.IllegalEntityException;
 import java.util.List;
 
 /**
@@ -19,8 +20,9 @@ public interface GoalDao {
      * @param goal to be created
      * @return created goal
      * @exception IllegalArgumentException when goal is null
+     * @exception IllegalEntityException when id of goal is not null or other attributes are null
      */
-    Goal createGoal(Goal goal)throws IllegalArgumentException;
+    Goal createGoal(Goal goal)throws IllegalArgumentException, IllegalEntityException;
     
     /**
      * Updates gived goal in database.
@@ -28,16 +30,18 @@ public interface GoalDao {
      * @param goal to be updated
      * @return updated goal
      * @exception IllegalArgumentException when goal is null
+     * @exception IllegalEntityException when some goal attribute is null
      */
-    Goal updateGoal(Goal goal) throws IllegalArgumentException;
+    Goal updateGoal(Goal goal) throws IllegalArgumentException, IllegalEntityException;
     
     /**
      * Deletes given goal in database.
      * 
      * @param goal to be deleted 
      * @exception  IllegalArgumentException when goal is null
+     * @exception IllegalEntityException when goal has null id or does not exist in the database
      */
-    void deleteGoal(Goal goal) throws IllegalArgumentException;
+    void deleteGoal(Goal goal) throws IllegalArgumentException, IllegalEntityException;
     
     /**
      * Retrieves goal with given id.
@@ -54,8 +58,9 @@ public interface GoalDao {
      * @param match where were goals shot
      * @return list of goals with given match
      * @exception IllegalArgumentException when match is null
+     * @exception IllegalEntityException when match id is null
      */
-    List<Goal> retrieveGoalsByMatch(Match match) throws IllegalArgumentException;
+    List<Goal> retrieveGoalsByMatch(Match match) throws IllegalArgumentException, IllegalEntityException;
     
     /**
      * Retrieves goals in given match shot by given team.
@@ -64,8 +69,9 @@ public interface GoalDao {
      * @param team which shot the goals 
      * @return list of goals with given match and team
      * @exception  IllegalArgumentException when match or team is null
+     * @exception IllegalEntityException when match or team id is null
      */
-    List<Goal> retrieveGoalsByMatchAndTeam(Match match, Team team) throws IllegalArgumentException;
+    List<Goal> retrieveGoalsByMatchAndTeam(Match match, Team team) throws IllegalArgumentException, IllegalEntityException;
     
     /**
      * Retrieves goals shot by given player
@@ -73,8 +79,9 @@ public interface GoalDao {
      * @param player who shot the goals
      * @return list of goals witch given player
      * @exception IllegalArgumentException when player is null
+     * @exception IllegalEntityException when player id is null
      */
-    List<Goal> retrieveGoalsByPlayer(Player player) throws IllegalArgumentException;
+    List<Goal> retrieveGoalsByPlayer(Player player) throws IllegalArgumentException, IllegalEntityException;
     
     /**
      * Retrieves goals shot in given match by given player
@@ -83,7 +90,8 @@ public interface GoalDao {
      * @param player who shot the goals
      * @return list of goals witch goven match and player
      * @exception IllegalArgumentException when match or player is null
+     * @exception IllegalEntityException when match or player id is null
      */
-    List<Goal> retrieveGoalsByMatchAndPlayer(Match match, Player player) throws IllegalArgumentException;
+    List<Goal> retrieveGoalsByMatchAndPlayer(Match match, Player player) throws IllegalArgumentException, IllegalEntityException;
     
 }
