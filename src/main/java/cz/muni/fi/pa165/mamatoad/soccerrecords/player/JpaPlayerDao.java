da4a7b736115dc7c74622c17ffe1ecd7b2d4f950
maros-klimovsky <395858@mail.muni.cz>
2013-10-10 18:57:06 +0200

    Added JpaPlayerDao.java Changed return types in PlayerDao.java for create and update to void Added @throws to appropriate methods in PlayerDao.java

---
+++ src/main/java/cz/muni/fi/pa165/mamatoad/soccerrecords/player/JpaPlayerDao.java
@@ -0,0 +1,129 @@
+package cz.muni.fi.pa165.mamatoad.soccerrecords.player;
+
+import cz.muni.fi.pa165.mamatoad.soccerrecords.team.Team;
+import cz.muni.fi.pa165.mamatoad.soccerrecords.util.exception.IllegalEntityException;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+
+/**
+ * JPA implementation of PlayerDao interface.
+ * This interface provides create, retrieve, update and delete methods for work with Player entity.
+ * @author Maros Klimovsky
+ */
+public class JpaPlayerDao implements PlayerDao {
+
+    private EntityManagerFactory entityManagerFactory;
+    
+    public JpaPlayerDao(EntityManagerFactory entityManagerFactory) {
+        if(entityManagerFactory == null)
+            throw new IllegalArgumentException("entityManagerFactory cannot be null");
+        
+        this.entityManagerFactory = entityManagerFactory;
+    }
+    
+    @Override
+    public void create(Player player) throws IllegalEntityException {
+        if (player == null)
+            throw new IllegalArgumentException("player cannot be null");
+        
+        if (player.getId() != null)
+            throw new IllegalEntityException("player.id is already set");
+        
+        if (player.getName() == null || player.getName().isEmpty())
+            throw new IllegalEntityException("player.name cannot be null");
+        
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(player);
+        entityManager.getTransaction().commit();
+                
+    }
+
+    @Override
+    public void update(Player player) throws IllegalEntityException {
+        if (player == null)
+            throw new IllegalArgumentException("player cannot be null");
+        
+        if (player.getName() == null || player.getName().isEmpty())
+            throw new IllegalEntityException("player.name cannot be null");
+        
+        if (player.getId() == null)
+            throw new IllegalEntityException("player.id cannot be null");
+        
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        
+        entityManager.getTransaction().begin();
+        entityManager.merge(player);
+        entityManager.getTransaction().commit();
+        
+    }
+
+    @Override
+    public void delete(Player player) throws IllegalEntityException {
+        if (player == null)
+            throw new IllegalArgumentException("player cannot be null");
+        
+        if (player.getId() == null)
+            throw new IllegalEntityException("player.id cannot be null");
+        
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.remove(player);
+        entityManager.getTransaction().commit();
+        
+    }
+
+    @Override
+    public Player retrievePlayerById(Long id) {
+        if (id == null)
+            throw new IllegalArgumentException("id cannot be null");
+        
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        
+        Player player = entityManager.find(Player.class,id);
+        
+        return player;
+    }
+
+    @Override
+    public List<Player> retrievePlayersByName(String name) {
+        if (name == null || name.isEmpty())
+            throw new IllegalArgumentException("name cannot be null");
+        
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        
+        TypedQuery<Player> query = entityManager.createQuery("Select p from Player p Where p.name=:name", Player.class).setParameter(":name", name);
+        Player player = query.getSingleResult();
+        List<Player> players = query.getResultList();
+        
+        return players;
+    }
+
+    @Override
+    public List<Player> retrievePlayersByTeam(Team team) throws IllegalEntityException {
+        if (team == null)
+            throw new IllegalArgumentException("team cannot be null");
+        
+        if (team.getId() == null)
+            throw new IllegalEntityException("team.id cannot be null");
+        
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        
+        TypedQuery<Player> query = entityManager.createQuery("Select p from Player p Where p.team=:team", Player.class).setParameter(":team", team);
+        List<Player> players = query.getResultList();
+        
+        return players;
+    }
+
+    @Override
+    public List<Player> retrievePlayersByActivity(boolean active) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        
+        TypedQuery<Player> query = entityManager.createQuery("Select p from Player p Where p.active=:active", Player.class).setParameter(":active", active);
+        List<Player> players = query.getResultList();
+        
+        return players;
+    }
+}
--- src/main/java/cz/muni/fi/pa165/mamatoad/soccerrecords/player/PlayerDao.java
+++ src/main/java/cz/muni/fi/pa165/mamatoad/soccerrecords/player/PlayerDao.java
@@ -1,6 +1,7 @@
 package cz.muni.fi.pa165.mamatoad.soccerrecords.player;
 
 import cz.muni.fi.pa165.mamatoad.soccerrecords.team.Team;
+import cz.muni.fi.pa165.mamatoad.soccerrecords.util.exception.IllegalEntityException;
 import java.util.List;
 
 /**
@@ -13,49 +14,58 @@ public interface PlayerDao {
      * This method creates new Player
      * @param player to create
      * @return created player
+     * @throws IllegalArgumentException if player is null
+     * @throws IllegalEntityException if player.id is already set or player.name is not set
      */
-    Player create(Player player);
+    void create(Player player) throws IllegalEntityException, IllegalArgumentException;
     
     /**
      * This method updates player
      * @param player to update
      * @return updated player
+     * @throws IllegalArgumentException if player is null
+     * @throws IllegalEntityException if player.id or player.name is not set
      */
-    Player update(Player player);
+    void update(Player player) throws IllegalEntityException, IllegalArgumentException;
     
     /**
      * This method deletes player
      * @param player to delete
+     * @throws IllegalArgumentException if player is null
+     * @throws IllegalEntityException if player.id is not set
      */
-    void delete(Player player);
+    void delete(Player player) throws IllegalEntityException, IllegalArgumentException;
     
     /**
      * This method retrieves player by his id
      * @param id of player
-     * @return player with given id
+     * @return player with given id or null if not found
+     * @throws IllegalArgumentException if id is null
      */
-    Player retrievePlayerById(Long id);
+    Player retrievePlayerById(Long id) throws IllegalArgumentException;
     
     /**
      * This method retrieves player by name
      * @param name of player
      * @return list of players with given name
+     * @throws IllegalArgumentException if name is not set
      */
-    List<Player> retrievePlayersByName(String name);
+    List<Player> retrievePlayersByName(String name) throws IllegalArgumentException;
     
     /**
      * This method retrieves player by team
      * @param team of player
      * @return list of players with given team
+     * 
      */
-    List<Player> retrievePlayersByTeam(Team team);
+    List<Player> retrievePlayersByTeam(Team team) throws IllegalEntityException, IllegalArgumentException;
     
     /**
      * This method retrieves player by activity
      * @param active - is player active?
      * @return list of players with given activity
      */
-    List<Player> retrievePlayersByActivity(Boolean active);
+    List<Player> retrievePlayersByActivity(boolean active) throws IllegalArgumentException;
     
     
 }
