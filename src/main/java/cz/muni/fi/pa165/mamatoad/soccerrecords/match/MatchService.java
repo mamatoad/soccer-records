74097ea2a5d41fd923d65bc039642a4a22e6b160
Adriana <adriana.smijakova@gmail.com>
2013-10-27 10:28:58 +0100

    Added MatchService.java

---
+++ src/main/java/cz/muni/fi/pa165/mamatoad/soccerrecords/match/MatchService.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.mamatoad.soccerrecords.match;
+
+import cz.muni.fi.pa165.mamatoad.soccerrecords.goal.GoalTO;
+import java.util.List;
+
+/**
+ * This interface allows to manipulate with matches.
+ *
+ * @author Adriana Smijakova
+ */
+public interface MatchService {
+    
+    /**
+     * Adds new record for match
+     * 
+     * @param match match to be created
+     */
+    public void add(MatchTO match);
+    /**
+     * Updates match with match.id, sets values from object match
+     * 
+     * @param match match to update
+     */
+    public void update(MatchTO match);
+    /**
+     * Removed match
+     * 
+     * @param match  match to delete
+     */
+    public void remove(MatchTO match);
+    
+    /**
+     * Returns list of goals shot in match
+     * 
+     * @param match match where the goals were shot
+     * @return list of all goals shot in this match
+     */
+    public List<GoalTO> getGoals(MatchTO match);
+    
+}
