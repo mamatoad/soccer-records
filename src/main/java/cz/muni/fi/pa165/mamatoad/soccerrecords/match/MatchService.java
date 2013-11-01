9a927a3bf8808a240fd7c399cb35125ef39dd14c
Adriana <adriana.smijakova@gmail.com>
2013-11-01 21:13:53 +0100

    Add exceptions to MatchService documentation

--- src/main/java/cz/muni/fi/pa165/mamatoad/soccerrecords/match/MatchService.java
+++ src/main/java/cz/muni/fi/pa165/mamatoad/soccerrecords/match/MatchService.java
@@ -17,18 +17,23 @@ public interface MatchService {
      * Adds new record for match
      * 
      * @param match match to be created
+     * @exception IllegalArgumentException when match is null
      */
     public void add(MatchTO match);
+    
     /**
      * Updates match with match.id, sets values from object match
      * 
      * @param match match to update
+     * @exception IllegalArgumentException when match is null
      */
     public void update(MatchTO match);
+    
     /**
      * Removed match
      * 
      * @param match  match to delete
+     * @exception IllegalArgumentException when match is null
      */
     public void remove(MatchTO match);
     
@@ -37,6 +42,7 @@ public interface MatchService {
      * 
      * @param teamId if of the team
      * @return list of matchTO where played team with given id
+     * @exception IllegalArgumentException when teamId is null
      */
     public List<MatchTO> getMatchesByTeamId(Long teamId);
     
