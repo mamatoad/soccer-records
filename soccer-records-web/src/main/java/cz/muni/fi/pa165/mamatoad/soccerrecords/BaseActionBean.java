52ee2e76c84f3cf94654b87ef32f6ab806a5b204
maros-klimovsky <395858@mail.muni.cz>
2013-11-13 23:28:59 +0100

    Added stripes
    added stripes 1.5.7 dependency
    added BaseActionBean as ancestor for all action beans

---
+++ soccer-records-web/src/main/java/cz/muni/fi/pa165/mamatoad/soccerrecords/BaseActionBean.java
@@ -0,0 +1,24 @@
+package cz.muni.fi.pa165.mamatoad.soccerrecords;
+
+import net.sourceforge.stripes.action.ActionBean;
+import net.sourceforge.stripes.action.ActionBeanContext;
+
+/**
+ * Base actionBean implementing the required methods for setting and getting context.
+ *
+ * @author Maros Klimovsky
+ */
+public abstract class BaseActionBean implements ActionBean {
+    private ActionBeanContext context;
+
+    @Override
+    public void setContext(ActionBeanContext context) {
+        this.context = context;
+    }
+
+    @Override
+    public ActionBeanContext getContext() {
+        return context;
+    }
+
+}
